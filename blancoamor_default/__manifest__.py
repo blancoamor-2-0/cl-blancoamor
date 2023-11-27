3523a53c74799be9b7f59fa9ea03c33d04bf8320
Gabriela Rivero <regaby@gmail.com>
2023-11-27 19:10:07 -0300

    [UPD] paso repositorio filoquin/ux a github

--- blancoamor_default/__manifest__.py
+++ blancoamor_default/__manifest__.py
@@ -48,7 +48,8 @@
         'git@github.com:blancoamor-2-0/ba_campus.git blancoamor/ba_campus',
         # 'git@code.gestionblancoamor.com:filoquin/website_themes.git varios/website_themes',
         'git@github.com:blancoamor-2-0/website_themes.git varios/website_themes',
-        'git@code.gestionblancoamor.com:filoquin/ux.git varios/ba-ux',
+        # 'git@code.gestionblancoamor.com:filoquin/ux.git varios/ba-ux',
+        'git@github.com:blancoamor-2-0/ba_ux.git varios/ba-ux',
         'git@code.gestionblancoamor.com:odoo-13/ks_dashboard_ninja.git varios/ks_dashboard_ninja',
 
         # mercado pago
