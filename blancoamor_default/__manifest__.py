d765ad35bb69dd34aded34939a57dcb294410904
Gabriela Rivero <regaby@gmail.com>
2023-11-27 19:23:07 -0300

    [UPD] paso repositorio ks_dashboard_ninja a github

--- blancoamor_default/__manifest__.py
+++ blancoamor_default/__manifest__.py
@@ -44,13 +44,10 @@
         'git@github.com:blancoamor-2-0/ba_website.git blancoamor/ba_website',
         'git@github.com:blancoamor-2-0/ba_hr.git blancoamor/ba_hr',
 
-        # 'git@code.gestionblancoamor.com:odoo-13/ba_campus.git blancoamor/ba_hr',
         'git@github.com:blancoamor-2-0/ba_campus.git blancoamor/ba_campus',
-        # 'git@code.gestionblancoamor.com:filoquin/website_themes.git varios/website_themes',
         'git@github.com:blancoamor-2-0/website_themes.git varios/website_themes',
-        # 'git@code.gestionblancoamor.com:filoquin/ux.git varios/ba-ux',
         'git@github.com:blancoamor-2-0/ba_ux.git varios/ba-ux',
-        'git@code.gestionblancoamor.com:odoo-13/ks_dashboard_ninja.git varios/ks_dashboard_ninja',
+        'git@github.com:blancoamor-2-0/ks_dashboard_ninja.git varios/ks_dashboard_ninja',
 
         # mercado pago
         'https://github.com/plugberry/mercadopago.git -b 13.0 varios/mercadopago',
